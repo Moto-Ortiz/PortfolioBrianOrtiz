7f878f2d32ce6c7bc5d8cfe1019d0d35564c6884
Moto-Ortiz <motobortiz@gmail.com>
2022-05-21 17:37:26 -0300

    backend
    creacion del archivo back en springboot

---
+++ PortfolioBrianOrtiz/BackEnd/src/main/java/com/portfoliobrianortiz/BrianOrtiz/BrianOrtizApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliobrianortiz.BrianOrtiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrianOrtizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrianOrtizApplication.class, args);
+	}
+
+}
---
+++ PortfolioBrianOrtiz/BackEnd/src/test/java/com/portfoliobrianortiz/BrianOrtiz/BrianOrtizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliobrianortiz.BrianOrtiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrianOrtizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

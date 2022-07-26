f25e646d7c921fec9332320a2d4a8250e3234a91
Moto-Ortiz <motobortiz@gmail.com>
2022-07-26 12:22:28 -0300

    implementacion jwt en el front
    Con visual studio code implementé el jwt de netbeans

---
+++ PortfolioBrianOrtiz/FrontEnd/src/app/model/login-usuario.ts
@@ -0,0 +1,11 @@
+import { timeStamp } from "console";
+
+export class LoginUsuario {
+    nombreUsuario : string;
+    password : string;
+
+    constructor(nombreUsuario: string, password: string){
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+    }
+}
